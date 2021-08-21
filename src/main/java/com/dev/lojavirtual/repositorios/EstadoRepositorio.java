b689578a697e78944018011953b7169c877717f4
IgorLuigi <igor.luigi@hotmail.com>
2021-08-21 16:16:08 -0300

    Cadastro de Estados

---
+++ src/main/java/com/dev/lojavirtual/repositorios/EstadoRepositorio.java
@@ -0,0 +1,9 @@
+package com.dev.lojavirtual.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.lojavirtual.modelos.Estado;
+
+public interface EstadoRepositorio extends JpaRepository<Estado, Long>{
+
+}
