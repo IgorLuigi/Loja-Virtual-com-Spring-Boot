ff1038047d7939a6e2490a5ead024eadf4d8a54b
IgorLuigi <igor.luigi@hotmail.com>
2021-08-21 16:49:30 -0300

    Cadastro de Cidades

---
+++ src/main/java/com/dev/lojavirtual/repositorios/CidadeRepositorio.java
@@ -0,0 +1,9 @@
+package com.dev.lojavirtual.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.lojavirtual.modelos.Cidade;
+
+public interface CidadeRepositorio extends JpaRepository<Cidade, Long>{
+
+}
