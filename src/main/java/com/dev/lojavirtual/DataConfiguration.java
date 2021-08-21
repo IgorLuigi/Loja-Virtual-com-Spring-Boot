ea63aeec35d21624c8b8e21b66318fef9db3676f
IgorLuigi <igor.luigi@hotmail.com>
2021-08-21 01:37:59 -0300

    alterações

--- src/main/java/com/dev/lojavirtual/DataConfiguration.java
+++ src/main/java/com/dev/lojavirtual/DataConfiguration.java
@@ -16,7 +16,7 @@ public class DataConfiguration {
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/lojagoodbits?useTimezone=true&serverTimezone=UTC");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/lojavirtual?useTimezone=true&serverTimezone=UTC");
 		dataSource.setUsername("root");
 		dataSource.setPassword("");
 		return dataSource;
