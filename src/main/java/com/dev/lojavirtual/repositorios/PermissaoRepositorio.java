ac7259ac0ab6a44fed3a609c4962bdbbb08f4aa9
IgorLuigi <igor.luigi@hotmail.com>
2021-11-02 01:11:57 -0300

    Cadastro de Permissões

---
+++ src/main/java/com/dev/lojavirtual/repositorios/PermissaoRepositorio.java
@@ -0,0 +1,9 @@
+package com.dev.lojavirtual.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.lojavirtual.modelos.Permissao;
+
+public interface PermissaoRepositorio extends JpaRepository<Permissao, Long>{
+
+}
